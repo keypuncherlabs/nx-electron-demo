262cf561392584bc3596dd52869ebd7313b3ff33
keypuncherlabs <cmarkovich@starscraper.io>
2022-07-16 10:52:09 -0500

    Adding electron

---
+++ apps/electron-demo/src/app/constants.ts
@@ -0,0 +1,4 @@
+export const rendererAppPort = 4200;
+export const rendererAppName = 'react-desktop-ui';
+export const electronAppName = 'electron-demo';
+export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
